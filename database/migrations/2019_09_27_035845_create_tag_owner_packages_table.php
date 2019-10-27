6e3c477453ebbcd99349072520989b7ae1776ac8
Ying Ying <YingYing@Yings-MacBook-Air-2.local>
2019-10-27 18:27:07 +0800

    package modify

---
+++ database/migrations/2019_09_27_035845_create_tag_owner_packages_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagOwnerPackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_owner_packages', function (Blueprint $table) {
+            $table->bigIncrements('owner_packages_id');
+            $table->unsignedBigInteger('users_id');
+            $table->unsignedBigInteger('packages_id');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->timestamps();
+            $table->foreign('users_id')
+            ->references('users_id')
+            ->on('users')->onDelete('cascade');
+            $table->foreign('packages_id')
+            ->references('packages_id')
+            ->on('packages')->onDelete('cascade');
+            
+        });
+      }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag_owner_packages');
+    }
+}
