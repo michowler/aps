d3a1f53c1cfcb06d106eac4909c48d3424b7c477
Michelle Ler <michelleler@Michelles-MacBook-Pro-2.local>
2019-11-03 02:13:16 +0800

    delete user with text input working

--- resources/views/partials/delete-user-modal.blade.php
+++ resources/views/partials/delete-user-modal.blade.php
@@ -1,28 +1,41 @@
-<div class="modal modal-danger fade" id="deleteUserModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal modal-danger fade" id="deleteVoucherModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog">
         <div class="modal-content">
-
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                <h4 class="modal-title" id="myModalLabel">Delete Account?</h4>
+                <h4 class="modal-title" id="myModalLabel">Delete Confirmation</h4>
             </div>
-            <form id="deleteUserForm" action="{{route(
-            'deleteUser', '$user->users_id')}}" method="POST">
+            <form id="deleteVoucherForm" action="{{route(
+            'deleteVoucher', '$voucher->vouchers_id')}}" method="POST">
                 @method('POST')
                 @csrf
             <div class="modal-body">                
                 <p>Are you sure you, want to delete your account?</p>
                 <p>Please copy the following sentence to delete account.</p>
-                <p>I confirm to delete my account</p>
-                <input type="text" name="deleteText" placeholder="Copy the text above here">
-                <input type="hidden" name="users_id" id="users_id" value="{{$user->users_id}}">
-            </div>                                        
-            <div class="modal-footer">                    
-                <button type="button" class="btn btn-sm btn-default" data-dismiss="modal">No, Cancel</button>
-                <button type="submit" class="btn btn-sm btn-primary" id="btn btn-sm btn-danger" onclick="formSubmit()">Yes, Delete</button>
+                <p>"I confirm to delete"</p>
+                <input type="text" name="deleteText" id="delUsrText" placeholder="Copy the text above here">
             </div>
+            
+                <input type="hidden" name="vouchers_id" id="vouchers_id" value="{{$voucher->vouchers_id}}">
+                <div class="modal-footer">                    
+                    <button type="button" class="btn btn-sm btn-default" data-dismiss="modal">No, Cancel</button>
+                    <button type="submit" class="btn btn-sm btn-danger"  id="delUsrbtn" onclick="formSubmit()">Yes, Delete</button>
+                </div>
             </form>
         </div>
     </div>
 </div>
 
+<script type="text/javascript">
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById('delUsrbtn').disabled = "true";
+    $("input[name=deleteText]").on('input',function(){        
+        var text = "I confirm to delete";
+        if($(this).val() === text){                          
+          document.getElementById('delUsrbtn').removeAttribute("disabled");
+        }else{
+          document.getElementById('delUsrbtn').disabled = "true";
+        }              
+    });
+});
+</script>
