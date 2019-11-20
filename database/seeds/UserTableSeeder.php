d7b3cca27203d047951a64132472b9c0bbbdb3a5
Michelle Ler <michelleler@Michelles-MacBook-Pro-2.local>
2019-11-20 13:39:58 +0800

    fix db seed locations

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -28,6 +28,16 @@ class UserTableSeeder extends Seeder
         // $user = User::create(['id' => 2, 'blah' => 'honk']);
         // $users[$user->id] = $user;        
 
+        $user = User::create(['name'=> 'tes', 'email' => 'test@gmail.com', 'password' => bcrypt('123123123'), 'email_verified_at' => now(), 'remember_token' => Str::random(10)]);
+
+        $user2 = User::create(['name'=> 'test2', 'email' => 'test2@gmail.com', 'password' => bcrypt('123123123'), 'email_verified_at' => now(), 'remember_token' => Str::random(10)]);
+
+        $user3 = User::create(['name'=> 'test3', 'email' => 'test3@gmail.com', 'password' => bcrypt('123123123'), 'email_verified_at' => now(), 'remember_token' => Str::random(10)]);
+
+        $user4 = User::create(['name'=> 'test4', 'email' => 'test4@gmail.com', 'password' => bcrypt('123123123'), 'email_verified_at' => now(), 'remember_token' => Str::random(10)]);
+
+        $user5 = User::create(['name'=> 'test5', 'email' => 'test5@gmail.com', 'password' => bcrypt('123123123'), 'email_verified_at' => now(), 'remember_token' => Str::random(10)]);
+
         // $roles = [];
         $role3 = Role::where('roles_id', '=', 3)->get();          
         $role2 = Role::where('roles_id', '=', 2)->get(); 
@@ -39,6 +49,12 @@ class UserTableSeeder extends Seeder
         $res->roles()->attach($role1);
         // $res->vouchers()->attach($voucher1);
         // $user[1]->roles()->sync([2,3]);        
+
+        $user->roles()->attach($role); //sync roles_id
+        $user2->roles()->attach($role2);
+        $user3->roles()->attach($role3);
+        $user4->roles()->attach($role4);
+        $user5->roles()->attach($role5);
     }
 
 }
