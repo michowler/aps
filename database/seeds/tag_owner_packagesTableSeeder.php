79b7ec59a9f55adf475387fc4b812f892ebf2b35
Michelle Ler <michelleler@Michelles-MacBook-Pro-2.local>
2019-11-24 16:15:56 +0800

    edit seed file

--- database/seeds/tag_owner_packagesTableSeeder.php
+++ database/seeds/tag_owner_packagesTableSeeder.php
@@ -14,7 +14,7 @@ class tag_owner_packagesTableSeeder extends Seeder
     public function run()
     {
         
-        $owner_packages = [['owner_packages_id' => '1','users_id' => '1'],['owner_packages_id' => '2','users_id' => '2'],['owner_packages_id' => '3','users_id' => '3'],['owner_packages_id' => '4','users_id' => '4'],['owner_packages_id' => '5','users_id' => '5']];
+        $owner_packages = [['owner_packages_id' => '1','users_id' => '2'],['owner_packages_id' => '2','users_id' => '4'],['owner_packages_id' => '3','users_id' => '5'],['owner_packages_id' => '4','users_id' => '6'],['owner_packages_id' => '5','users_id' => '7'], ['owner_packages_id' => '6','users_id' => '8']];
       
         \DB::table('tag_owner_packages') -> insert($owner_packages);
     }       
